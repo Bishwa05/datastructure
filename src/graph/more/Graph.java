221f4f3ec3711f548427a256cfc09c6b9861d8d2
Bishwa <bishwa.nayak@sap.com>
2020-03-18 23:21:25 +0530

    few more ds

--- src/graph/more/Graph.java
+++ src/graph/more/Graph.java
@@ -1,8 +1,6 @@
 package graph.more;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class Graph {
     private int V;
@@ -19,100 +17,103 @@ public class Graph {
         }
     }
 
-    public Graph(In in) {
-        if (in == null) throw new IllegalArgumentException("argument is null");
+    public Graph() {
         try {
-            this.V = in.readInt();
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Number of Vertex");
+
+            this.V = sc.nextInt();
             if (V < 0) throw new IllegalArgumentException("number of vertices in a Graph must be nonnegative");
-            adj = (Bag<Integer>[]) new Bag[V];
+            adj = (List<Integer>[]) new ArrayList[V];
             for (int v = 0; v < V; v++) {
-                adj[v] = new Bag<Integer>();
+                adj[v] = new ArrayList<Integer>();
             }
-            int E = in.readInt();
+            System.out.println("Enter Number of Edges");
+            int E = sc.nextInt();
             if (E < 0) throw new IllegalArgumentException("number of edges in a Graph must be nonnegative");
             for (int i = 0; i < E; i++) {
-                int v = in.readInt();
-                int w = in.readInt();
+                int v = sc.nextInt();
+                int w = sc.nextInt();
                 validateVertex(v);
                 validateVertex(w);
                 addEdge(v, w);
             }
-        }
-        catch (NoSuchElementException e) {
+        } catch (NoSuchElementException e) {
             throw new IllegalArgumentException("invalid input format in Graph constructor", e);
         }
+    }
 
-        public Graph(Graph G) {
-            this.V = G.V();
-            this.E = G.E();
-            if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+    public Graph(Graph G) {
+        this.V = G.V();
+        this.E = G.E();
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
 
-            // update adjacency lists
-            adj = (Bag<Integer>[]) new Bag[V];
-            for (int v = 0; v < V; v++) {
-                adj[v] = new Bag<Integer>();
+        // update adjacency lists
+        adj = (List<Integer>[]) new ArrayList[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new ArrayList<Integer>();
+        }
+
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            Stack<Integer> reverse = new Stack<Integer>();
+            for (int w : G.adj[v]) {
+                reverse.push(w);
             }
-
-            for (int v = 0; v < G.V(); v++) {
-                // reverse so that adjacency list is in same order as original
-                Stack<Integer> reverse = new Stack<Integer>();
-                for (int w : G.adj[v]) {
-                    reverse.push(w);
-                }
-                for (int w : reverse) {
-                    adj[v].add(w);
-                }
+            for (int w : reverse) {
+                adj[v].add(w);
             }
         }
+    }
 
-        public int V() {
-            return V;
-        }
+    public int V() {
+        return V;
+    }
 
-        public int E() {
-            return E;
-        }
+    public int E() {
+        return E;
+    }
 
-        private void validateVertex(int v) {
-            if (v < 0 || v >= V)
-                throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
-        }
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
 
-        public void addEdge(int v, int w) {
-            validateVertex(v);
-            validateVertex(w);
-            E++;
-            adj[v].add(w);
-            adj[w].add(v);
-        }
+    public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
 
-        public Iterable<Integer> adj(int v) {
-            validateVertex(v);
-            return adj[v];
-        }
+    public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
 
-        public int degree(int v) {
-            validateVertex(v);
-            return adj[v].size();
-        }
+    public int degree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
 
-        public String toString() {
-            StringBuilder s = new StringBuilder();
-            s.append(V + " vertices, " + E + " edges " + "\n");
-            for (int v = 0; v < V; v++) {
-                s.append(v + ": ");
-                for (int w : adj[v]) {
-                    s.append(w + " ");
-                }
-                s.append("\n");
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + "\n");
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (int w : adj[v]) {
+                s.append(w + " ");
             }
-            return s.toString();
+            s.append("\n");
         }
+        return s.toString();
+    }
 
-        public static void main(String[] args) {
-            In in = new In(args[0]);
-            Graph G = new Graph(in);
-            StdOut.println(G);
-        }
+    public static void main(String[] args) {
+        Graph G = new Graph();
+        System.out.println(G);
+    }
 
     }
