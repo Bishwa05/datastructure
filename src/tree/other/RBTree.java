880b4e867ecd4b864c88a1b1dda93616cec2125b
Bishwa <bishwa.nayak@sap.com>
2020-03-23 23:43:30 +0530

    RB Tree

--- src/tree/other/RBTree.java
+++ src/tree/other/RBTree.java
@@ -19,6 +19,64 @@ package tree.other;
  *
  *
  */
-public class RBTree {
+public class RBTree <Key extends Comparable<Key>, Value>{
+    private static final boolean RED   = true;
+    private static final boolean BLACK = false;
+    private Node root;
+
+    private class Node {
+        private Key key;           // key
+        private Value val;         // associated data
+        private Node left, right;  // links to left and right subtrees
+        private boolean color;     // color of parent link
+        private int size;          // subtree count
+
+        public Node(Key key, Value val, boolean color, int size) {
+            this.key = key;
+            this.val = val;
+            this.color = color;
+            this.size = size;
+        }
+    }
+
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.size;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    /***************************************************************************
+     *  Standard BST search.
+     ***************************************************************************/
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        return get(root, key);
+    }
+
+    // value associated with the given key in subtree rooted at x; null if no such key
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if      (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else              return x.val;
+        }
+        return null;
+    }
+
+
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
 
 }
