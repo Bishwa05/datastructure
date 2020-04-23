e2214fe81f3dc86da8d7d1673f113effab1703ce
Bishwa <bishwa.nayak@sap.com>
2020-04-23 23:06:33 +0530

    Trie leetcode

---
+++ src/tree/trie/WordDictionary.java
@@ -0,0 +1,94 @@
+package tree.trie;
+
+/**
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
+ *
+ */
+public class WordDictionary {
+    TrieNode root;
+    class TrieNode{
+        boolean isLeaf;
+        TrieNode[] children;
+
+        TrieNode(){
+            children = new TrieNode[26];
+            isLeaf= false;
+        }
+    }
+
+    /**
+     * The commented code for leetcode
+     *
+     *     public WordDictionary() {
+     *         root = new TrieNode();
+     *     }
+     *
+     */
+
+    public void addWord(String word){
+        if(root== null){
+            root = new TrieNode();
+        }
+        TrieNode curr = root;
+        for(char c : word.toCharArray()){
+            if(curr.children[c-'a']==null){
+                curr.children[c-'a']= new TrieNode();
+            }
+            curr = curr.children[c-'a'];
+        }
+        curr.isLeaf = true;
+    }
+    public boolean searchUtil(String word, TrieNode curr) {
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(c=='.') {
+                String newWord = word.substring(i+1);
+
+                boolean found = false;
+                for(int j =0; j<26; j++){
+                    if (curr.children[j] != null) {
+                        found = searchUtil(newWord, curr.children[j]);
+                    }
+
+                    if(found){
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else {
+                if (curr.children[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.children[c - 'a'];
+            }
+        }
+        return curr != null && curr.isLeaf;
+    }
+
+
+    public boolean search(String word){
+
+        TrieNode curr = root;
+        return searchUtil(word,curr);
+
+
+    }
+
+    public static void main(String arg[]){
+        WordDictionary w = new WordDictionary();
+        w.addWord("bad");
+        w.addWord("dad");
+        w.addWord("mad");
+        System.out.println(w.search("pad")); // -> false
+        System.out.println(w.search("bad")); //-> true
+        System.out.println(w.search(".an")); //-> false
+        System.out.println(w.search("b..")); //-> true
+    }
+}
