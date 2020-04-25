987a03ba73d44dbff07c54ce06b6567968c291f0
Bishwa <bishwa.nayak@sap.com>
2020-04-25 20:33:54 +0530

    Queue

--- src/queue/MyCircularQueueOptimized.java
+++ src/queue/MyCircularQueueOptimized.java
@@ -1,58 +1,81 @@
 package queue;
 
-public class MyCircularQueueOptimized {
+class MyCircularQueueOptimized {
+
+    private int[] data;
+    private int head;
+    private int tail;
+    private int size;
 
     /** Initialize your data structure here. Set the size of the queue to be k. */
-    int[] q;
-    int start=0, end=0, len, count=0;
     public MyCircularQueueOptimized(int k) {
-        q=new int[k];
-        len=k;
+        data = new int[k];
+        head = -1;
+        tail = -1;
+        size = k;
     }
 
     /** Insert an element into the circular queue. Return true if the operation is successful. */
     public boolean enQueue(int value) {
-        if((end+1)%len == start) return false;
-        if(count==0) {
-            q[end]=value;
+        if (isFull() == true) {
+            return false;
         }
-        else{
-            end=(end+1)%len;
-            q[end] = value;
+        if (isEmpty() == true) {
+            head = 0;
         }
-        count++;
+        tail = (tail + 1) % size;
+        data[tail] = value;
         return true;
     }
 
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     public boolean deQueue() {
-        if(count==0) return false;
-        count--;
-        if(count!=0) start++;
+        if (isEmpty() == true) {
+            return false;
+        }
+        if (head == tail) {
+            head = -1;
+            tail = -1;
+            return true;
+        }
+        head = (head + 1) % size;
         return true;
     }
 
     /** Get the front item from the queue. */
     public int Front() {
-        if(count==0) return -1;
-        return q[start];
+        if (isEmpty() == true) {
+            return -1;
+        }
+        return data[head];
     }
 
     /** Get the last item from the queue. */
     public int Rear() {
-        if(count==0) return -1;
-        return q[end];
+        if (isEmpty() == true) {
+            return -1;
+        }
+        return data[tail];
     }
 
     /** Checks whether the circular queue is empty or not. */
     public boolean isEmpty() {
-        if(count==0) return true;
-        return false;
+        return head == -1;
     }
 
     /** Checks whether the circular queue is full or not. */
     public boolean isFull() {
-        if(count==len) return true;
-        return false;
+        return ((tail + 1) % size) == head;
     }
 }
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
